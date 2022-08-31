b4a6d73c8e7da9e31b0c89e2defd097eaec0803d
Yuri <12345@mail.ru>
2022-08-31 21:39:04 +0400

    Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, маша");
+}
+else
+{
+	Console.Write("Привет, ");
+	Console.WriteLine(username);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
